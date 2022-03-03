f06a6f3c7749d26c9fe8e38719c2acdba847e8c5
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-03 14:43:18 +1100

    Apply max-width

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import Header from '../Header';
+import Board from '../Board';
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <Board />
     </div>
   );
 };
