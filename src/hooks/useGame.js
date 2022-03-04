263273931943d7f277143eb3675494e43264b9a7
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2022-03-04 15:09:02 +1100

    Define generateWord and validateWord methods

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,4 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import randomWord from 'random-word-by-length';
 
 const GameContext = createContext();
 
@@ -10,6 +12,27 @@ export const GameProvider = ({ children }) => {
   const [ wordLength, setWordLength ] = useState(5);
   const [ maxGuesses, setMaxGuesses ] = useState(6);
   const [ error, setError ] = useState('');
+
+  useEffect(() => {
+    generateWord();
+  }, [])
+
+  const generateWord = () => {
+    let word = randomWord(wordLength);
+
+    // randomWord() only takes a max length argument, so it could be shorter than the required length
+    while (answer.length !== wordLength && validateWord(answer)) {
+      word = randomWord(wordLength);
+    }
+
+    setAnswer(word);
+  };
+
+  const validateWord = (word) => {
+    return axios(`https://api.dictionaryapi.dev/api/v2/entries/en/${ word }`)
+      .then(() => true)
+      .catch(() => false);
+  };
 
   const values = {
     answer,
